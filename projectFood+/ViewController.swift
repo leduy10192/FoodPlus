32d10244c50e416c061a1ccfc6506b3d7b681753
Ge Ou <ougejiajia@gmail.com>
2020-04-19 15:32:49 -0700

    Initial Commit

---
+++ projectFood+/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  projectFood+
+//
+//  Created by Ge Ou on 2020/4/19.
+//  Copyright © 2020 Ge Ou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
